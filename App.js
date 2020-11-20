8f4731d572c096199a341b87b4916de7c1d7d5b3
ryong890314 <ryong890314@gmail.com>
2020-11-20 17:11:34 +0900

    expo 적용

---
+++ App.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.root}>
+        <View style={styles.statusbar}>
+           <StatusBar style="light" />
+        </View>
+        <View style={styles.browserContainer}>
+          <WebView
+          useWebKit = { true }
+          source={{ uri: 'https://ryong890314.github.io/app-lample/' }}
+          style={{ marginTop: 0 }}
+         />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1, 
+  },
+  statusbar: {
+      flex: 0,
+  },
+  browserContainer: {
+      flex: 2,
+  }
+});
